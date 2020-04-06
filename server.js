8b3379d96cf0f30d0fe2d4680310af96458fc296
U-DESKTOP-GSCEPCV\jpbea <jpbeaudet@vn3t.com>
2020-04-05 20:13:18 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,62 @@
+// Author: Jean-Philippe Beaudet @ S3R3NITY Technology 
+//
+// /server.js
+// Version : 0.0.1
+// License: GNU 3.0 General Public License
+//
+// =====================================================
+
+// load the things we need
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var path = require('path');
+var router = express.Router();
+
+var server = require('http').createServer(app);
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(require('express-session')({
+    secret: 'HODLsecret',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(express.static(path.join(__dirname, 'assets')));
+app.use(express.static(path.join(__dirname, 'data')));
+
+
+// index page 
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+// explorer tx page 
+app.get('/tx/:txid', function(req, res) {
+	var data = {txid: req.params.txid}
+    res.render('pages/tx', data);
+});
+
+// explorer addresses page 
+app.get('/address/:address', function(req, res) {
+	var data = {address: req.params.address}
+    res.render('pages/address', data);
+});
+
+// trading 
+app.get('/trade', function(req, res) {
+    res.render('pages/trade');
+});
+
+//  support 
+app.get('/support', function(req, res) {
+    res.render('pages/support');
+});
+
+//start server
+server.listen(8080);
+console.log('server started on port: 8080');
