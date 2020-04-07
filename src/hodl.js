05a102a48e48c9f0dad11f50e9780d3050f98631
U-DESKTOP-GSCEPCV\jpbea <jpbeaudet@vn3t.com>
2020-04-07 11:47:36 -0400

    added infuria support

--- src/hodl.js
+++ src/hodl.js
@@ -1,13 +1,17 @@
 /*
-Public address of the key:   0x3F252493e949bBAc22cDd9D9f220F9645F2fF923
-Path of the secret key file: C:\Users\jpbea\AppData\Local\Ethereum\keystore\UTC--2020-04-06T22-07-55.432867100Z--3f252493e949bbac22cdd9d9f220f9645f2ff923
+ * author : Jean-Philippe Beaudet
+ * jpbeaudet@vsekur.com
+ * License GNU 3.0
+ * Infuria project ID :
+* 94841089cf24457ebaab3d909f3558d6
 */
+
 var Web3 = require('web3');
 var solc = require("solc");
 var fs = require('fs');
 
-// Connect to a geth server over JSON-RPC
-var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:30303"));
+// Connect to a Infuria server over JSON-RPC
+var web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/94841089cf24457ebaab3d909f3558d6"));
 var input = {
   language: 'Solidity',
   sources: {
@@ -23,40 +27,25 @@ var input = {
     }
   }
 };
-console.log("Talking with a geth server", web3.version.api);
+console.log("Powered by Infuria");
 
-// Read standard token contract from https://www.ethereum.org/token
-//var sourceCode = fs.readFileSync('./src/hodl/hodl.sol','UTF-8')
-//var compiled = solc.compile(JSON.stringify(input));
-//console.log(compiled)
 var output = JSON.parse(solc.compile(JSON.stringify(input)));
-//console.log(output.contracts['hodl']["HODL"].abi)
 var abiArray = output.contracts['hodl']["HODL"].abi;
-//abiArray = JSON.parse(abiArray);
 var address = "0x34642A469e096138531E6396C50D2d416d0B47D7"
 // Create a proxy object to access the smart contract
 var MyContract = new web3.eth.Contract(abiArray);
 MyContract.options.address = address
-console.log(MyContract.methods.totalSupply().call( {from: "0xA4ab16FA7F26aa482f2736f386533A406f7B9183"},async(error, result)=>{
+console.log(MyContract.methods.totalSupply().call( async(error, result)=>{
 	if(error){
 		console.log("Err:"+error)
 	}else{
 		console.log(result)
 	}
-	}))
-// instantiate by address
-;
-//var contractInstance = MyContract.at(address);
-
-// All public variables have automatically generated getters
-// http://bitcoin.stackexchange.com/a/38079/5464
-
-//console.log(JSON.stringify(result));
-	
+	}));	
 module.exports = {
     public: function(){
-		var contractInstance = MyContract.methods;
-
+		// set contractInstance.methods to make transactionobject creation easy
+		var contractInstance = MyContract.methods;;
 		// All public variables have automatically generated getters
 		// http://bitcoin.stackexchange.com/a/38079/5464
 		var result = {
